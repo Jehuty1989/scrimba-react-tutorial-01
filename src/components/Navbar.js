dc4c51964e015a170dc155698067cd823c1990cd
Alastair Turner <specta1989@gmail.com>
2022-03-27 19:56:43 +0100

    Added style and refactored code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ import logo from '../images/logo192.png'
 
 export default function Navbar() {
   return (
-    <div className="nav-bar">
+    <nav className="nav-bar">
       <div className="nav-bar-left">
         <img src={logo} alt="React Logo" />
         <h3>ReactFacts</h3>
       </div>
       <h4>React Course - Project 1</h4>
-    </div>
+    </nav>
   )
 }
